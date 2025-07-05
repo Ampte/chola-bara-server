d2b9986ed8c3ae4b5fd73bedf46a5984bbfde0ec
Ampte B Marak <amptebmarak@gmail.com>
2025-07-05 22:02:32 +0530

    Connect to database

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mysql = require("mysql");
 
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+db.connect((error) => {
+    if(error){
+        console.log("Cannot connect to database", error);
+    }else{
+        console.log("Connected to database");
+    };
+});
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
